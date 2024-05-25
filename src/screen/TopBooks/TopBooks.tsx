042738629252fdf54593f3479d1b4648e25c6c60
Kaim Arvas <skaimarvas@gmail.com>
2024-05-25 15:56:00 +0300

    add mapping

--- src/screen/TopBooks/TopBooks.tsx
+++ src/screen/TopBooks/TopBooks.tsx
@@ -1,21 +1,37 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import PageNavigator from '../../components/PageNavigator';
 import BookItem from '../../components/BookItem';
+import {bookList} from '../../mock/BookListData';
 
 const TopBooks = () => {
   return (
-    <View>
-      <PageNavigator />
-      <Text style={styles.text}>TopBooks</Text>
-      <BookItem />
-    </View>
+    <ScrollView>
+      <PageNavigator screenName={`Trending Books`} />
+      <View style={styles.bookList}>
+        {bookList &&
+          bookList.map((book, i) => (
+            <BookItem
+              key={i}
+              name={book.name}
+              author={book.author}
+              image={book.image}
+            />
+          ))}
+      </View>
+    </ScrollView>
   );
 };
 
 export default TopBooks;
 
 const styles = StyleSheet.create({
+  layout: {
+    flex: 1,
+  },
+  bookList: {
+    paddingTop: 20,
+  },
   text: {
     color: 'black',
   },
