e27f390a3884b8cb11345ee8cf8ed36483dd0cf8
Kaim Arvas <skaimarvas@gmail.com>
2024-06-30 15:08:25 +0300

    add ApiProvider

---
+++ src/api/ApiProvider.tsx
@@ -0,0 +1,64 @@
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import axios from 'axios';
+import React, {useMemo} from 'react';
+import {StyleSheet} from 'react-native';
+import {MMKV} from 'react-native-mmkv';
+import {ScreenParams} from '../../App';
+
+export const storage = new MMKV();
+
+export const BASE_URL = '...';
+
+axios.defaults.timeout = 10000;
+
+// axios.defaults.baseURL = BASE_URL
+
+interface Iprops {
+  children: any;
+}
+
+const ApiProvider: React.FC<Iprops> = ({children}) => {
+  const navigation = useNavigation<
+    NativeStackNavigationProp<ScreenParams> | any
+  >();
+
+  useMemo(() => {
+    axios.interceptors.request.use(
+      async (config: any) => {
+        const token = storage.getString('mybookapp:token');
+        axios.defaults.headers.common['Accept-Language'] = 'tr';
+        if (token) {
+          config.headers['Authorization'] = `Bearer ${token}`;
+        }
+        return config;
+      },
+      async (error: any) => {
+        return Promise.reject(error);
+      },
+    );
+    axios.interceptors.response.use(
+      async (response: any) => {
+        if (response.data.token) {
+          storage.set('mybookapp:token', response?.data?.token);
+        }
+        return response;
+      },
+      (error: any) => {
+        const statusCode = error.response ? error.response.status : null;
+        const detailMessage = error.response.data;
+
+        switch (statusCode) {
+          default:
+            return;
+        }
+      },
+    );
+  }, []);
+
+  return children;
+};
+
+export default ApiProvider;
+
+const styles = StyleSheet.create({});
