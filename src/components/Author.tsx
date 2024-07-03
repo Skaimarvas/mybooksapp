42a6988d2447e61aa82bd0dba2b86c61c3bbc3e3
Kaim Arvas <skaimarvas@gmail.com>
2024-07-03 14:52:03 +0300

    add shadow

--- src/components/Author.tsx
+++ src/components/Author.tsx
@@ -9,7 +9,9 @@ interface Iprops {
 const Author: React.FC<Iprops> = ({image, authorName}) => {
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={image} />
+      <View style={styles.authorSection}>
+        <Image style={styles.image} source={image} />
+      </View>
       <Text style={styles.text}>{authorName}</Text>
     </View>
   );
@@ -31,6 +33,14 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: 1,
     resizeMode: 'contain',
+  },
+  authorSection: {
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 10},
+    shadowOpacity: 1,
+    shadowRadius: 100,
+    elevation: 4,
+    borderRadius: 100,
   },
   text: {
     color: 'black',
