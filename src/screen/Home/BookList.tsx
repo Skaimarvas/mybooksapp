d6aa23d5b4c7df40c178c3ef44918d6efab9715f
Kaim Arvas <skaimarvas@gmail.com>
2024-07-03 14:53:22 +0300

    add shadows

--- src/screen/Home/BookList.tsx
+++ src/screen/Home/BookList.tsx
@@ -1,40 +1,86 @@
 import React from 'react';
-import {Image, ScrollView, StyleSheet, TouchableOpacity} from 'react-native';
+import {
+  Dimensions,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {bookList} from '../../mock/BookListData';
 import {useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {ScreenParams} from '../../../App';
+import PageNavigator from '../../components/PageNavigator';
+
+const screenWidth = Dimensions.get('screen').width;
 
 const BookList: React.FC = () => {
   const navigation = useNavigation<NativeStackNavigationProp<ScreenParams>>();
   return (
-    <ScrollView contentContainerStyle={{}} style={styles.bookSection}>
-      {bookList &&
-        bookList.map((book: any, i: number) => (
-          <TouchableOpacity
-            key={i}
-            onPress={() => navigation.navigate('BookDetails')}>
-            <Image
-              style={[styles.bookImage, {height: i == 1 ? 280 : 280}]}
-              source={book.image}
-            />
-          </TouchableOpacity>
-        ))}
-    </ScrollView>
+    <View>
+      <PageNavigator title="Curated Pick" />
+      <ScrollView
+        contentContainerStyle={styles.container}
+        style={styles.bookSection}>
+        {bookList &&
+          bookList.map((book: any, i: number) => (
+            <TouchableOpacity
+              key={i}
+              onPress={() => navigation.navigate('BookDetails')}>
+              <View style={styles.bookItem}>
+                <View style={styles.bookShadow}>
+                  <Image style={styles.bookImage} source={book.image} />
+                </View>
+                <Text style={styles.bookNameText}>{book.name}</Text>
+              </View>
+            </TouchableOpacity>
+          ))}
+      </ScrollView>
+    </View>
   );
 };
 
 export default BookList;
 
 const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    width: screenWidth,
+    justifyContent: 'center',
+    gap: 12,
+    paddingBottom: 80,
+  },
   textColor: {
     color: 'black',
   },
   bookSection: {
     width: '100%',
   },
+  bookItem: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 4,
+  },
+  bookShadow: {
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 10},
+    shadowOpacity: 1,
+    shadowRadius: 20,
+    elevation: 2,
+    borderRadius: 20,
+  },
+
+  bookNameText: {
+    color: 'black',
+    fontWeight: 'bold',
+  },
   bookImage: {
-    width: 160,
+    width: 190,
+    height: 330,
     borderRadius: 20,
   },
 });
